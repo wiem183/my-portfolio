2b724d9466ba22611fa61f683ff1075ee5d964e7
wiem183 <wiem.errouissi@esprit.tn>
2025-06-04 22:41:21 +0100

    wiem

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/my-portfolio',
-  assetPrefix: '/my-portfolio/',
 }
 
 module.exports = nextConfig
