2ef8467acbc6e0896e9a9b8afdb52ff0fc5a68bb
wiem183 <wiem.errouissi@esprit.tn>
2025-06-04 22:00:35 +0100

    Préparation déploiement GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/my-portfolio',
+  assetPrefix: '/my-portfolio/',
 }
 
 module.exports = nextConfig
